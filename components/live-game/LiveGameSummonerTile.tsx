336043830910cb091cb3e5bc36d5a52e36685dae
drneko17 <dr.neko17@gmail.com>
2022-09-07 15:11:03 +0200

    Update live match

--- components/live-game/LiveGameSummonerTile.tsx
+++ components/live-game/LiveGameSummonerTile.tsx
@@ -3,16 +3,41 @@ import { useEffect, useCallback, useState } from "react";
 import useGetChampionIconUrl from "../../hooks/useGetChampionIconUrl";
 import useGetSummonerSpellUrl from "../../hooks/useGetSummonerSpellUrl";
 import Image from "next/image";
+import Link from "next/link";
 
-import { LiveMatchParticipantType } from "../../types/summonerTypes";
+import {
+  LiveMatchParticipantType,
+  SummonerByIdType,
+} from "../../types/summonerTypes";
 import useGetRunesByIdArray from "../../hooks/useGetRunesByIdArray";
+import useGetEmblem from "../../hooks/useGetEmblem";
+import { useRouter } from "next/router";
 
 const LiveGameSummonerTile: React.FC<{
   data: LiveMatchParticipantType;
   classes: string;
 }> = ({ data, classes }) => {
+  const router = useRouter();
+  const { summonerId } = data;
+
   const [championIcon, setChampionIcon] = useState("");
+  const [emblemUrl, setEmblemUrl] = useState("");
   const [runes, setRunes] = useState([]);
+  const [thisSummonerData, setThisSummonerData] = useState({
+    leagueId: "",
+    queueType: "",
+    tier: "",
+    rank: "",
+    summonerId: "",
+    summonerName: "",
+    leaguePoints: 0,
+    wins: 0,
+    losses: 0,
+    veteran: false,
+    inactive: false,
+    freshBllot: false,
+    hotStreak: false,
+  });
 
   const summonerSpell1Url = useGetSummonerSpellUrl(data.spell1Id);
   const summonerSpell2Url = useGetSummonerSpellUrl(data.spell2Id);
@@ -27,9 +52,27 @@ const LiveGameSummonerTile: React.FC<{
     setRunes(foundRunes);
   }, []);
 
+  const summonerDataHandler = useCallback(async () => {
+    const response = await fetch("/api/hello", {
+      method: "GET",
+      headers: {
+        summonerid: `${summonerId}`,
+        server: `${router.query.server}`,
+      },
+    });
+
+    const responseData = await response.json();
+    const thisSummoner = responseData.leagueData.find(
+      (item: SummonerByIdType) => item.summonerId === summonerId
+    );
+    setEmblemUrl(useGetEmblem(thisSummoner.tier));
+    setThisSummonerData(thisSummoner);
+  }, []);
+
   useEffect(() => {
     championIconUrlHandler();
     runesHandler();
+    summonerDataHandler();
   }, []);
 
   return (
@@ -37,7 +80,11 @@ const LiveGameSummonerTile: React.FC<{
       <div
         className={`${classes} rounded-xl flex flex-col w-48 drop-shadow-lg items-center`}
       >
-        <div>{data.summonerName}</div>
+        <Link href={`/summoner/${router.query.server}/${data.summonerName}`}>
+          <div className="hover:underline hover:cursor-pointer text-lg">
+            {data.summonerName}
+          </div>
+        </Link>
         <div className="flex items-center space-x-1">
           <Image
             src={championIcon}
@@ -61,6 +108,24 @@ const LiveGameSummonerTile: React.FC<{
               className="rounded-xl drop-shadow-lg"
               layout="fixed"
             />
+          </div>
+        </div>
+        <div className="flex flex-col items-center mt-4">
+          <Image src={emblemUrl} width={128} height={128} />
+          <div className="">
+            {thisSummonerData.tier} {thisSummonerData.rank} -{" "}
+            {thisSummonerData.leaguePoints} LP
+          </div>
+          <div>
+            {thisSummonerData.wins}W/{thisSummonerData.losses}L
+          </div>
+          <div>
+            {(
+              (thisSummonerData.wins /
+                (thisSummonerData.wins + thisSummonerData.losses)) *
+              100
+            ).toFixed(2)}
+            % win ratio
           </div>
         </div>
         {/* RUNES */}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
+import formatUnicorn from "format-unicorn/safe";
+
+import { LEAGUE_BY_SUMMONER_ID } from "../../public/constants";
+import { SummonerByIdType } from "../../types/summonerTypes";
+
+const headersConfig = {
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36",
+  "Accept-Language": "en-US,en;q=0.5",
+  "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+  Origin: "https://developer.riotgames.com",
+  "X-Riot-Token": `${process.env.RIOT_API_KEY}`,
 };
 
-export default function handler(
+type Data = {
+  leagueData: SummonerByIdType;
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const leagueResponse = await fetch(
+    formatUnicorn(LEAGUE_BY_SUMMONER_ID, {
+      summonerId: req.headers.summonerid,
+      server: req.headers.server,
+    }),
+    {
+      method: "GET",
+      headers: headersConfig,
+    }
+  );
+
+  const leagueData: SummonerByIdType = await leagueResponse.json();
+
+  res.status(200).json({ leagueData });
 }
